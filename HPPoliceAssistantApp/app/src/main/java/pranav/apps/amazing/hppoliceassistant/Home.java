cdd1f492ec55e523340a394ec17d431260607277
pranavgupta1234 <pranavgupta4321@gmail.com>
2017-01-17 17:01:32 +0530

    press back again to exit feature added and activity stack issues corrected

--- HPPoliceAssistantApp/app/src/main/java/pranav/apps/amazing/hppoliceassistant/Home.java
+++ HPPoliceAssistantApp/app/src/main/java/pranav/apps/amazing/hppoliceassistant/Home.java
@@ -3,9 +3,12 @@ package pranav.apps.amazing.hppoliceassistant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by Pranav Gupta on 12/18/2016.
@@ -14,6 +17,7 @@ import android.widget.ImageButton;
 
 public class Home extends Activity{
     private ImageButton entry,challan,stolen_list,search_vehicle;
+    boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,5 +58,25 @@ public class Home extends Activity{
                 startActivity(i);
             }
         });
+    }
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click back again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
